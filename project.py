a8049666f8b4168f892a2ea79f3d356e935f1f76
Eka <eka.abashidze@gmail.com>
2023-09-05 17:36:17 +0400

    feat: add dictionary to save the information about users financial details

--- project.py
+++ project.py
@@ -1,12 +1,15 @@
 def main():
-    get_info();
+    financial_data = []
+    data = get_info();
+    print(data)
+    
 
 
 def get_info():
     expenses = int(input("How much was your expenses last month? "))
     income = int(input("What was your income last month? "))
     savings = int(input("How much savings did you make last month? "))
-    print(expenses, income, savings)
+    return {"expenses": expenses, "income": income, "savings": savings};
 
 def function_2():
     ...
