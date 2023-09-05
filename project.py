6b44fa88e222279f5fb84365893177f2e5877467
Eka <eka.abashidze@gmail.com>
2023-09-05 17:31:51 +0400

    feat: create prompting users for input functionality about their expenses, income and savings

--- project.py
+++ project.py
@@ -1,10 +1,12 @@
 def main():
-    ...
+    get_info();
 
 
-def function_1():
-    ...
-
+def get_info():
+    expenses = int(input("How much was your expenses last month? "))
+    income = int(input("What was your income last month? "))
+    savings = int(input("How much savings did you make last month? "))
+    print(expenses, income, savings)
 
 def function_2():
     ...
