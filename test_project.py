6b6a7a4d400d9603dfcc258ec4cfd3e2e836c33b
Eka <eka.abashidze@gmail.com>
2023-09-18 16:01:37 +0400

    feat: add first test for getting information as integer testing

--- test_project.py
+++ test_project.py
@@ -1,10 +1,22 @@
-def test_function_1():
-    ...
+from project import get_info
+from project import get_percentages
+from project import calculate_net_income
+
+def test_get_info():
+    data = get_info()
+    assert "income" in data 
+    assert isinstance(data["income"], int)
+    assert "income" in data 
+    assert isinstance(data["income"], int)
+    assert "expenses" in data 
+    assert isinstance(data["expenses"], int)
+    assert "savings" in data 
+    assert isinstance(data["savings"], int)
 
 
-def test_function_2():
-    ...
+# def test_get_percentages():
+#     ...
 
 
-def test_function_n():
-    ...
+# def test_calculate_net_income():
+#     ...
