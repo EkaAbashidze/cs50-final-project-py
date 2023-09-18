40ed35c4339aba6a4025734239e738f2a6a9847b
Eka <eka.abashidze@gmail.com>
2023-09-18 16:31:58 +0400

    feat: add second testing function to test get percentages function

--- test_project.py
+++ test_project.py
@@ -14,9 +14,14 @@ def test_get_info():
     assert isinstance(data["savings"], int)
 
 
-# def test_get_percentages():
-#     ...
+def test_get_percentages():
+    data = {"income": 1000, "expenses": 300, "savings": 200}
 
+    expected_expense_percentage = (data["expenses"] / data["income"] * 100)
+    expected_savings_percentage = (data["savings"] / data["income"] * 100)
+    
+    assert expected_expense_percentage == 30
+    assert expected_savings_percentage == 20
 
 # def test_calculate_net_income():
 #     ...
